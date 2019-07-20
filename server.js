f921cd625f02b5e86d27443496e417676d99291b
Luis Martinez <martinez.s.luis@gmail.com>
2019-07-20 18:53:51 +0200

    Extended signin

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("IT'S WORKING");
 });
-app.post("/signin", signin.handleSignin(db, bcrypt));
+app.post("/signin", signin.signinAuthentication(db, bcrypt));
 app.post("/register", (req, res) => {
   register.handleRegister(req, res, db, bcrypt);
 });
